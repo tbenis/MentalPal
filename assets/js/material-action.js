6c087d2d1c64e4b281cc935b68cf439562bab84f
tbenis <will.jenifer@yahoo.com>
2019-11-09 22:27:13 -0500

    Mental Pal first commit

---
+++ assets/js/material-action.js
@@ -0,0 +1,7 @@
+ $(document).ready(function(){
+     alert("Hello");
+   $('.collapsible').collapsible();
+   //accordion = true;
+    // var instance = M.Collapsible.getInstance(elem);
+    // instance.open(2);
+ });
